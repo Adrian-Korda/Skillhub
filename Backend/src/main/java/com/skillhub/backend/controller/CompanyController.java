d8c2f220f2750ae8ed818988c19f47a806025870
AdrianKorda <adriankorda6@gmail.com>
2026-02-13 15:05:03 +0100

    add global webconfig for cors

--- Backend/src/main/java/com/skillhub/backend/controller/CompanyController.java
+++ Backend/src/main/java/com/skillhub/backend/controller/CompanyController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = { "http://localhost:5173", "https://skillhub-project.vercel.app" })
 @RequestMapping("/api/companies")
 public class CompanyController {
 
--- Backend/src/main/java/com/skillhub/backend/controller/TrendsController.java
+++ Backend/src/main/java/com/skillhub/backend/controller/TrendsController.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = { "http://localhost:5173", "https://skillhub-project.vercel.app" })
 @RequestMapping("/api/jobs")
 public class TrendsController {
 
